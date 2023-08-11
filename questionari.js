2039b736dbd42265c8f4f80e88392fdcfda9e460
LINIL08 <107807849+LINIL08@users.noreply.github.com>
2023-08-11 09:54:55 +0200

    Add files via upload

--- questionari.js
+++ questionari.js
@@ -1,32 +1,37 @@
 const questions = [
     {
-        question: "Quin any som?",
-        answers: ["2023"],
+        question: "Quina és la capital de França?",
+        answers: ["París", "Paris"],
+        hint: "És coneguda com la 'ciutat de l'amor'.",
         link: "https://ca.wikipedia.org/wiki/Par%C3%ADs"
     },
     {
-        question: "Com et dius?",
-        answers: ["Bruna", "Raluy"],
+        question: "Quin gas és necessari perquè els vegetals realitzin la fotosíntesi?",
+        answers: ["Diòxid de carboni", "CO2"],
+        hint: "Té la fórmula química CO2.",
         link: "https://ca.wikipedia.org/wiki/Fotos%C3%ADntesi"
     },
     {
         question: "Quin és el riu més llarg del món?",
         answers: ["Níl", "Nil"],
-        link: "https://ca.wikipedia.org/wiki/Nil"
+        hint: "És famós per ser el riu de l'Egipte antic.",
+        link: "https://ca.wikipedia.org/wiki/N%C3%ADl"
     }
 ];
 
 const questionElement = document.getElementById('question');
 const answerElement = document.getElementById('answer');
-const linkElement = document.getElementById('link');
+const hintElement = document.getElementById('hint');
 const submitButton = document.getElementById('submit');
+const hintLinkElement = document.getElementById('hint-link');
 
 let currentQuestionIndex = 0;
 
 function displayQuestion(index) {
     questionElement.textContent = questions[index].question;
     answerElement.value = '';
-    linkElement.style.display = 'none';
+    hintElement.style.display = 'none';
+    hintLinkElement.style.display = 'inline-block';
 }
 
 function checkAnswer(index, answer) {
@@ -34,15 +39,15 @@ function checkAnswer(index, answer) {
     return validAnswers.includes(answer.trim().toLowerCase());
 }
 
-function goToLink(link) {
-    window.open(link, '_blank');
+function displayHint(index) {
+    hintElement.textContent = "Pista: " + questions[index].hint;
+    hintElement.style.display = 'block';
+    hintLinkElement.style.display = 'none';
 }
 
 function handleAnswerSubmission() {
     const userAnswer = answerElement.value;
     if (checkAnswer(currentQuestionIndex, userAnswer)) {
-        const link = questions[currentQuestionIndex].link;
-        goToLink(link);
         if (currentQuestionIndex < questions.length - 1) {
             currentQuestionIndex++;
             displayQuestion(currentQuestionIndex);
@@ -60,4 +65,10 @@ answerElement.addEventListener('keydown', function (event) {
         event.preventDefault();
         handleAnswerSubmission();
     }
+});
+
+hintLinkElement.addEventListener('click', function (event) {
+    event.preventDefault();
+    const link = questions[currentQuestionIndex].link;
+    window.open(link, '_blank');
 });
